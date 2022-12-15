c7c460283551d3c7ab726cc05fc49182bc3d38f9
santosguih <guilherme_henriques@outlook.com>
2022-12-14 22:44:24 -0300

    teste commit

--- share_bill/lib/main.dart
+++ share_bill/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
@@ -11,7 +11,7 @@ void main() async{
   );
 
   FirebaseFirestore.instance;
-  runApp(const MyApp());  
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ShareBill',
+      title: 'ShareBillie',
       theme: ThemeData(
         // This is the theme of your application.
         //
